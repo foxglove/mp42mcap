fb8dd8d3947a1ca09a49f8cf7244bb3a4be60126
Adrian Macneil <adrian@foxglove.dev>
2024-11-07 20:48:22 -0800

    Iterate over frames

--- src/main.rs
+++ src/main.rs
@@ -28,25 +28,44 @@ fn main() -> Result<(), ffmpeg::Error> {
     ffmpeg::init()?;
 
     // Open the input file
-    let input = ffmpeg::format::input(&cli.input)?;
+    let mut input = ffmpeg::format::input(&cli.input)?;
 
-    // Find the best video stream
-    let video_stream = input
+    // Find the best video stream and store its index
+    let video_stream_index = input
         .streams()
         .best(ffmpeg::media::Type::Video)
-        .ok_or(ffmpeg::Error::StreamNotFound)?;
+        .ok_or(ffmpeg::Error::StreamNotFound)?
+        .index();
 
-    // Get the decoder
-    let codec = ffmpeg::codec::context::Context::from_parameters(video_stream.parameters())?;
+    // Get the decoder from the stream parameters
+    let codec = ffmpeg::codec::context::Context::from_parameters(
+        input.streams().best(ffmpeg::media::Type::Video).unwrap().parameters()
+    )?;
     let mut decoder = codec.decoder().video()?;
 
-    // Now you can receive frames
+    // Iterate over packets
     let mut frame = ffmpeg::frame::Video::empty();
+    let mut packet_iter = input.packets();
+    while let Some((stream, packet)) = packet_iter.next() {
+        // Skip packets that aren't from our video stream
+        if stream.index() != video_stream_index {
+            continue;
+        }
 
-    while let Ok(_) = decoder.receive_frame(&mut frame) {
-        // Here you will process each frame and write to MCAP
-        // frame.data(0) contains the raw pixel data
+        // Send the packet to the decoder
+        decoder.send_packet(&packet)?;
+
+        // Receive all frames from this packet
+        while decoder.receive_frame(&mut frame).is_ok() {
+            println!("Processing frame: {}x{}", frame.width(), frame.height());
+        }
+    }
+
+    // Flush the decoder
+    decoder.send_eof()?;
+    while decoder.receive_frame(&mut frame).is_ok() {
         println!("Processing frame: {}x{}", frame.width(), frame.height());
+        // Process frame here
     }
 
     Ok(())
